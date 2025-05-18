92636a337923a7f5c5f86268e9c0d74e52077411
Patriot5701 <91824409+Patriot5701@users.noreply.github.com>
2025-05-18 21:04:52 +0200

    Ajout de config.js

---
+++ src/config.js
@@ -0,0 +1,18 @@
+export default {
+  "churchName": "",
+  "pages": {
+    "home": true,
+    "values": false,
+    "team": false,
+    "media": false,
+    "agenda": false,
+    "activities": false
+  },
+  "template": "classic",
+  "customTheme": {
+    "primary": "#3490dc",
+    "background": "#f7fafc",
+    "secondary": "#ffed4a"
+  },
+  "theme": "light"
+}
